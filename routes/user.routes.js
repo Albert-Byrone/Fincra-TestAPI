a89563658fbfd48fd26785900474bf9e62f8ede5
Albert Byrone <albertbyrone1677@gmail.com>
2024-02-28 13:33:11 +0300

    add user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,17 @@
 const express = require('express');
-const { registerUser } = require('../controller/user.controller');
+const { registerUser, loginUser, getUserProfile } = require('../controller/user.controller');
+const { authenticate } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
+// Route for adding  a new user
 router.post('/register', registerUser)
+
+// Route to login a user
+router.post('/login', loginUser)
+
+// Route to get the profile of the logged-in user
+router.get('/profile', authenticate, getUserProfile);
+
 
 module.exports = router;
