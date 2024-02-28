2a703c31a5fcab1c845e038ea84d2ba1361b4889
Albert Byrone <albertbyrone1677@gmail.com>
2024-02-28 14:00:37 +0300

    configure swagger documentation

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const commentRoute = require("./routes/comment.routes")
 const logger = require('./utils/logger'); // Adjust the path as necessary
 const { info } = require("console");
 
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
+
 // Load the environment
 require('dotenv').config();
 
@@ -28,6 +32,7 @@ app.use("/tickets", ticketRoute);
 app.use("/comments", commentRoute);
 
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 app.listen(port, () => {
---
+++ swagger.js
@@ -0,0 +1,25 @@
+const swaggerAutogen = require('swagger-autogen')();
+// const userRoute = require("./routes/user.routes")
+// const ticketRoute = require("./routes/ticket.routes")
+// const commentRoute = require("./routes/comment.routes")
+
+const doc = {
+  info: {
+    title: 'Fincra Test API Documentation',
+    description: 'Customer support ticketing system API documentation'
+  },
+  host: 'localhost:5000'
+};
+
+const outputFile = './swagger-output.json';
+const endpointFiles = '/app.js';
+const routes = ['./routes/user.routes', './routes/ticket.routes', './routes/comment.routes'];
+
+/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
+root file where the route starts, such as index.js, app.js, routes.js, etc ... */
+
+// swaggerAutogen(outputFile, routes, doc);
+
+swaggerAutogen(outputFile, routes, doc).then(() => {
+  require('./app.js'); // Your project's root file
+});
